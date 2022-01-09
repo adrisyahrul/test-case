29cd83ffb9b8c892b5448b140632bbb43287b04f
Adri Syahrul <adrydhelfa20@gmail.com>
2022-01-10 01:51:34 +0700

    Create model & migration

---
+++ database/migrations/2022_01_09_182409_create_order_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('order');
+            $table->string('item');
+            $table->integer('qty');
+            $table->integer('price');
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
