c98c5d5aeab0e2552260f46bb71cf3d21e183da7
Adri Syahrul <adrydhelfa20@gmail.com>
2022-01-10 16:58:33 +0700

    Initiate Templating

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,6 +53,22 @@
                                 </li>
                             @endif
                         @else
+                            <li>
+                                <a class="nav-link" href="{{ url('/home') }}">Home</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Master Data
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Data Customer</a>
+                                    <a class="dropdown-item" href="#">Data Item</a>
+                                </div>
+                            </li>
+                            <li>
+                                <a class="nav-link" href="#">Transaction Order</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
