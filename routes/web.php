03e43da709e6297d31792b5a7dce068b256bda71
Adri Syahrul <adrydhelfa20@gmail.com>
2022-01-11 16:11:30 +0700

    Fix Auth

--- routes/web.php
+++ routes/web.php
@@ -19,24 +19,28 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/home', 'App\Http\Controllers\QuerysatuController@index');
+    Route::get('/home', 'App\Http\Controllers\QuerysatuController@index');
 
-// route for data costumer
-Route::get('/data/customer', 'App\Http\Controllers\CustomerController@index');
-Route::post('/data/customer/add', 'App\Http\Controllers\CustomerController@store');
-Route::get('/data/customer/update/{id}', 'App\Http\Controllers\CustomerController@update');
-Route::put('/data/customer/edit/{id}', 'App\Http\Controllers\CustomerController@edit');
-Route::get('/data/customer/delete/{id}', 'App\Http\Controllers\CustomerController@delete');
+    // route for data costumer
+    Route::get('/data/customer', 'App\Http\Controllers\CustomerController@index');
+    Route::post('/data/customer/add', 'App\Http\Controllers\CustomerController@store');
+    Route::get('/data/customer/update/{id}', 'App\Http\Controllers\CustomerController@update');
+    Route::put('/data/customer/edit/{id}', 'App\Http\Controllers\CustomerController@edit');
+    Route::get('/data/customer/delete/{id}', 'App\Http\Controllers\CustomerController@delete');
 
-// route for data items
-Route::get('/data/item', 'App\Http\Controllers\ItemController@index');
-Route::post('/data/item/add', 'App\Http\Controllers\ItemController@store');
-Route::get('/data/item/update/{id}', 'App\Http\Controllers\ItemController@update');
-Route::put('/data/item/edit/{id}', 'App\Http\Controllers\ItemController@edit');
-Route::get('/data/item/delete/{id}', 'App\Http\Controllers\ItemController@delete');
+    // route for data items
+    Route::get('/data/item', 'App\Http\Controllers\ItemController@index');
+    Route::post('/data/item/add', 'App\Http\Controllers\ItemController@store');
+    Route::get('/data/item/update/{id}', 'App\Http\Controllers\ItemController@update');
+    Route::put('/data/item/edit/{id}', 'App\Http\Controllers\ItemController@edit');
+    Route::get('/data/item/delete/{id}', 'App\Http\Controllers\ItemController@delete');
 
-// route one to many
-Route::get('/transaction/order', 'App\Http\Controllers\OrderController@index');
-Route::get('/transaction/orderitem', 'App\Http\Controllers\OrderItemController@index');
+    // route one to many
+    Route::get('/transaction/order', 'App\Http\Controllers\OrderController@index');
+    Route::get('/transaction/orderitem', 'App\Http\Controllers\OrderItemController@index');
+});
+
+
